25d23015bea3feb6df6d5f338fdec5b0a7d4181f
AxossO <ahmadsayed142d@gmail.com>
2023-10-23 17:56:40 +0300

    Commit

--- src/components/HomeMovie.jsx
+++ src/components/HomeMovie.jsx
@@ -1,37 +1,38 @@
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { verfiy } from "../../Api";
+import { useSelector } from "react-redux";
 
-const HomeMovie = () => {
-  const dispatch = useDispatch();
+const HomeMovie = ({ movie, isActive }) => {
   const status = useSelector((state) => state.movie.status);
-  useEffect(() => {
-    dispatch(verfiy());
-    if (status === "loading") console.log("loading");
-    else if (status === "fulfilled") console.log("fullfiled");
-  }, [dispatch, status]);
+  const imgSrc = "https://image.tmdb.org/t/p/original/";
+  // if (status === "fulfilled") console.log("fullfiled");
+  // if (status === "loading") console.log("loading");
   return (
     <>
-      <div className="flex max-w-4xl mx-auto mt-14 justify-between items-center">
-        <div className="space-y-10">
-          <h1 className="text-8xl text-lime-800">title</h1>
-          <p className="max-w-xl text-sm">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque,
-            ducimus quod, voluptate esse nulla autem voluptas tempore eaque ea
-            natus quis, exercitationem facilis possimus? Quis sapiente voluptate
-            voluptatum laborum esse!
-          </p>
-          <div className="space-x-4 rounded-md ">
-            <button className="rounded-lg bg-red-700 px-6 py-2 shadow-xl">
-              Watch Now
-            </button>
-            <button className="rounded-lg border bg-slate-500 px-6 py-2 shadow-xl">
-              Watch Trailer
-            </button>
+      {status === "fulfilled" && (
+        <div
+          className={`w-full flex items-center justify-center `}
+        >
+          <div className="space-y-10">
+            <h1 className="text-8xl text-lime-800">{movie.original_title}</h1>
+            <p className="max-w-xl text-sm">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque,
+              ducimus quod, voluptate esse nulla autem voluptas tempore eaque ea
+              natus quis, exercitationem facilis possimus? Quis sapiente
+              voluptate voluptatum laborum esse!
+            </p>
+            <div className="space-x-4 rounded-md ">
+              <button className="rounded-lg bg-red-700 px-6 py-2 shadow-xl">
+                Watch Now
+              </button>
+              <button className="rounded-lg border bg-slate-500 px-6 py-2 shadow-xl">
+                Watch Trailer
+              </button>
+            </div>
+          </div>
+          <div>
+            <img src={imgSrc + movie.backdrop_path} alt="" />
           </div>
         </div>
-        <div>IMG</div>
-      </div>
+      )}
     </>
   );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,28 @@
 import HomeMovie from "../components/HomeMovie";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { verfiy } from "../../Api";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/swiper-bundle.css";
 
 const Home = () => {
+  const movie = useSelector((state) => state.movie.movie);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(verfiy());
+  }, []);
   return (
-    <>
-      <HomeMovie  />
-    </>
+    <div className=" flex-row overflow-hidden flex px-4 ">
+      <div className="relative flex-row flex mx-auto max-w-7xl overflow-hidden  ">
+        <Swiper loop={true}>
+          {movie.map((movie) => (
+            <SwiperSlide key={movie.id}>
+              <HomeMovie movie={movie} key={movie.id} />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+    </div>
   );
 };
 
