aa9a2ab3f3a78a567054a901f9d5ca00b248c5fb
AxossO <ahmadsayed142d@gmail.com>
2023-11-19 16:28:16 +0200

    Commit

--- src/components/MovieGrid.jsx
+++ src/components/MovieGrid.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import OnHoverMovie from "./OnHoverMovie";
 
 const MovieGrid = ({ movie }) => {
   const imgSrc = "https://image.tmdb.org/t/p/original/";
@@ -6,18 +7,14 @@ const MovieGrid = ({ movie }) => {
   return (
     <div className="hover:scale-110  duration-300 relative">
       <div className="">
-        <div
-          className={` ${
-            ishovered ? "block" : "block"
-          }bg-black opacity-25 absolute w-full h-full `}
-          onMouseEnter={() => setIsHovered(!ishovered)}
-          onMouseLeave={() => setIsHovered(ishovered)}
-        ></div>
         <img
-          className=" rounded-2xl hover:rounded-[2rem] duration-300  "
+          className=" rounded-2xl hover:rounded-[2rem] duration-300 relative  "
           src={imgSrc + movie.poster_path}
+          onMouseEnter={() => setIsHovered(true)}
+          onMouseLeave={() => setIsHovered(false)}
           alt=""
         ></img>
+        {ishovered ? <OnHoverMovie movie={movie} /> : ""}
       </div>
     </div>
   );
---
+++ src/components/OnHoverMovie.jsx
@@ -0,0 +1,9 @@
+const OnHoverMovie = ({ movie }) => {
+  return (
+    <div className="absolute w-full h-full bg-black ">
+      <div></div>
+    </div>
+  );
+};
+
+export default OnHoverMovie;
