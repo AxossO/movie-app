0e97b66b13cca15d8f1e9412348dc8090f9b008e
AxossO <ahmadsayed142d@gmail.com>
2023-11-20 17:45:41 +0200

    Commit

--- src/components/MovieGrid.jsx
+++ src/components/MovieGrid.jsx
@@ -5,13 +5,15 @@ const MovieGrid = ({ movie }) => {
   const imgSrc = "https://image.tmdb.org/t/p/original/";
   const [ishovered, setIsHovered] = useState(false);
   return (
-    <div className="hover:scale-110  duration-300 relative">
+    <div
+      className="hover:scale-110  duration-300 relative hover:rounded-[2rem] "
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
       <div className="">
         <img
-          className=" rounded-2xl hover:rounded-[2rem] duration-300 relative  "
+          className=" rounded-2xl  duration-300 "
           src={imgSrc + movie.poster_path}
-          onMouseEnter={() => setIsHovered(true)}
-          onMouseLeave={() => setIsHovered(false)}
           alt=""
         ></img>
         {ishovered ? <OnHoverMovie movie={movie} /> : ""}
