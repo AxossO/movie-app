b73e98dda03eb7092c125453416c193f99c6a3ca
AxossO <ahmadsayed142d@gmail.com>
2024-02-26 12:18:26 +0200

    Commit

--- Api.js
+++ Api.js
@@ -1,7 +1,7 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
-const apiKey = import.meta.env.API_KEY;
+// const apiKey = import.meta.env.API_KEY;
 // const accessToken = import.meta.env.VITE_ACCESS_TOKEN;
 const accessToken = process.env.ACCESS_TOKEN;
 const url = "https://api.themoviedb.org/3";
