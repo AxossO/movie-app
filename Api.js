e644eda98f7f7e9b6c59cfed1c264a31f3d291af
AxossO <ahmadsayed142d@gmail.com>
2024-03-03 19:36:17 +0200

    Commit

--- Api.js
+++ Api.js
@@ -1,118 +1,125 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
+import { defineConfig, loadEnv } from "vite";
 
 // const apiKey = import.meta.env.API_KEY;
-const accessToken = import.meta.env.VITE_ACCESS_TOKEN;
+
+// const accessToken = import.meta.env.VITE_ACCESS_TOKEN;
 // const accessToken = process.env.ACCESS_TOKEN;
-const url = "https://api.themoviedb.org/3";
-console.log(accessToken)
-const min = 1;
-const max = 500;
-const randomNumber = Math.floor(Math.random() * (max - min) + min);
-const options = {
-  method: "GET",
-  url: `${url}/movie/now_playing`,
-  params: { language: "en-US", page: "1" },
-  headers: {
-    accept: "application/json",
-    Authorization: accessToken,
-  },
+const pot = ({ mode }) => {
+  // Load app-level env vars to node-level env vars.
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+  const accessToken = process.env.ACCESS_TOKEN;
+
+  const url = "https://api.themoviedb.org/3";
+  console.log(accessToken);
+  const min = 1;
+  const max = 500;
+  const randomNumber = Math.floor(Math.random() * (max - min) + min);
+  const options = {
+    method: "GET",
+    url: `${url}/movie/now_playing`,
+    params: { language: "en-US", page: "1" },
+    headers: {
+      accept: "application/json",
+      Authorization: accessToken,
+    },
+  };
+  const upcomingOption = (index) => ({
+    method: "GET",
+    url: `${url}/movie/upcoming?page${index}`,
+    params: { page: index },
+    headers: {
+      accept: "application/json",
+      Authorization: accessToken,
+    },
+  });
+  const seriesOption = (index) => ({
+    method: "GET",
+    url: `${url}/tv/popular?page${index}`,
+    params: { page: index },
+    headers: {
+      accept: "application/json",
+      Authorization: accessToken,
+    },
+  });
+  const topRatedSeriesOption = (index) => ({
+    method: "GET",
+    url: `${url}/tv/top_rated?page${index}`,
+    params: { page: index },
+    headers: {
+      accept: "application/json",
+      Authorization: accessToken,
+    },
+  });
+  const topRatedMovieOption = (index) => ({
+    method: "GET",
+    url: `${url}/movie/top_rated?page${index}`,
+    params: { page: index },
+    headers: {
+      accept: "application/json",
+      Authorization: accessToken,
+    },
+  });
+  const popular = (index) => ({
+    method: "GET",
+    url: `${url}/movie/popular?page${index}`,
+    params: { page: index },
+    headers: {
+      accept: "application/json",
+      Authorization: accessToken,
+    },
+  });
+  const gettingId = (id, endpoint) => ({
+    method: "GET",
+    url: `${url}/${endpoint}/${id}`,
+    headers: {
+      accept: "application/json",
+      Authorization: accessToken,
+    },
+  });
+  const gettingCast = (id, endpoint) => ({
+    method: "GET",
+    url: `${url}/${endpoint}/${id}/credits`,
+    headers: {
+      accept: "application/json",
+      Authorization: accessToken,
+    },
+  });
+  const gettingVideos = (id, endpoint) => ({
+    method: "GET",
+    url: `${url}/${endpoint}/${id}/videos`,
+    // url: `${url}/${endpoint}/${id}/videos`,
+    headers: {
+      accept: "application/json",
+      Authorization: accessToken,
+    },
+  });
+  const gettingImages = (id, endpoint) => ({
+    method: "GET",
+    url: `${url}/${endpoint}/${id}/images`,
+    headers: {
+      accept: "application/json",
+      Authorization: accessToken,
+    },
+  });
+  const recomenditon = () => ({
+    method: "GET",
+    url: `${url}/discover/movie?page=${randomNumber}`,
+    headers: {
+      accept: "application/json",
+      Authorization: accessToken,
+    },
+  });
+  const search = (searchNameMovie) => ({
+    method: "GET",
+    url: `${url}/search/movie?query=${searchNameMovie}`,
+    headers: {
+      accept: "application/json",
+      Authorization: accessToken,
+    },
+  });
 };
-const upcomingOption = (index) => ({
-  method: "GET",
-  url: `${url}/movie/upcoming?page${index}`,
-  params: { page: index },
-  headers: {
-    accept: "application/json",
-    Authorization:
-      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxM2M4MmY1M2Q5OTkzM2EwYTRjYTNlN2JlMzgwMjkyMSIsInN1YiI6IjY1ZGIyOWU5Y2FiZmU0MDE4NmQxNDU0ZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.OvjQWMfiq3U8QPn43Z8ikW8cMfMOI4S6hmvhJPXUqGk",
-  },
-});
-const seriesOption = (index) => ({
-  method: "GET",
-  url: `${url}/tv/popular?page${index}`,
-  params: { page: index },
-  headers: {
-    accept: "application/json",
-    Authorization: accessToken,
-  },
-});
-const topRatedSeriesOption = (index) => ({
-  method: "GET",
-  url: `${url}/tv/top_rated?page${index}`,
-  params: { page: index },
-  headers: {
-    accept: "application/json",
-    Authorization: accessToken,
-  },
-});
-const topRatedMovieOption = (index) => ({
-  method: "GET",
-  url: `${url}/movie/top_rated?page${index}`,
-  params: { page: index },
-  headers: {
-    accept: "application/json",
-    Authorization: accessToken,
-  },
-});
-const popular = (index) => ({
-  method: "GET",
-  url: `${url}/movie/popular?page${index}`,
-  params: { page: index },
-  headers: {
-    accept: "application/json",
-    Authorization: accessToken,
-  },
-});
-const gettingId = (id, endpoint) => ({
-  method: "GET",
-  url: `${url}/${endpoint}/${id}`,
-  headers: {
-    accept: "application/json",
-    Authorization: accessToken,
-  },
-});
-const gettingCast = (id, endpoint) => ({
-  method: "GET",
-  url: `${url}/${endpoint}/${id}/credits`,
-  headers: {
-    accept: "application/json",
-    Authorization: accessToken,
-  },
-});
-const gettingVideos = (id, endpoint) => ({
-  method: "GET",
-  url: `${url}/${endpoint}/${id}/videos`,
-  // url: `${url}/${endpoint}/${id}/videos`,
-  headers: {
-    accept: "application/json",
-    Authorization: accessToken,
-  },
-});
-const gettingImages = (id, endpoint) => ({
-  method: "GET",
-  url: `${url}/${endpoint}/${id}/images`,
-  headers: {
-    accept: "application/json",
-    Authorization: accessToken,
-  },
-});
-const recomenditon = () => ({
-  method: "GET",
-  url: `${url}/discover/movie?page=${randomNumber}`,
-  headers: {
-    accept: "application/json",
-    Authorization: accessToken,
-  },
-});
-const search = (searchNameMovie) => ({
-  method: "GET",
-  url: `${url}/search/movie?query=${searchNameMovie}`,
-  headers: {
-    accept: "application/json",
-    Authorization: accessToken,
-  },
-});
 
 export const searchMovie = createAsyncThunk(
   "searching",
